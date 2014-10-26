af49c3d58c8a57170fe40e68ad92dbaea26f9ace
mjbp <mickperkins@gmail.com>
2014-10-26 19:18:07 +0000

    Restructure to remove separate build step

--- README.md
+++ README.md
@@ -1,9 +1,42 @@
-Cramond Island Causeway
-========
+# Cramond Island Causeway Crossing
+A demo app to display safe times to cross Cramond Island causeway.
 
-App to display safe times to cross causeways.
+[http://cramond.herokuapp.com/](http://cramond.herokuapp.com/)
+
+
+##What is this?
+Between 2008-2013 where nearly 600 people were rescued from Cramond Island by Queensferry lifeboat at great cost to the RNLI. At the time of writing the only notice of safe cordsing times is the small noticeboard at the causeway.
+
+This is a proof of concept app and therefore should not constitute your sole guide for planning a trip to Cramond Island. Crossing times are estimated based on the low tide times at the port of Leith. 
+
+Always check the safe crossing times on the noticeboard at the causeway.
 
 
 
-#Issues
-Times across nights need fixing
+Tide data licensing...
+
+All times in DST/BST???
+
+***
+Also check: http://www.ntslf.org/tides/tidepred?port=Leith
+
+##How does it work
+Based on the premise that it is safe to cross for period of four hours, two hours either side of low tide. 
+
+Low tide data from the nearest port (Leith) are scraped from http://www.thebeachguide.co.uk/south-scotland/lothian/cramond-weather.htm. The data is cleaned up and processed into a vertical 24 hour clockface showing the time for he current day and the safe times for the next week.
+
+*****
+Move the site building into index.js
+****
+
+
+
+
+
+## If you wish to run your own installation
+1. Fork this repo
+2. npm install all dependencies
+3. Run 'node build'
+
+Frontend js production file in /src, gulp task to minify it and put it in /js for build
+
--- index.js
+++ index.js
@@ -1,14 +1,86 @@
+/*global require, console, process*/
 var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    jade = require('jade'),
+    cramondTides = require('./cramondTides.js'),
+    dataFile = 'data/data.json',
+    tpl = 'template/template.jade',
+    data = {},
+    helpers = {
+        zeropad : function (n) {
+            return String('00'+n).substr(-2);
+        },
+        minutesToTime : function (n) {
+            return String(helpers.zeropad(helpers.minutesToHour(n))) + ':' + String(helpers.zeropad(n % 60));
+        },
+        minutesToHour : function (n) {
+            return Math.floor(n / 60);
+        }
+    };
+
+checkData(readData);
+
+function checkData (cb) {
+    fs.stat(dataFile, function (err, stats) {
+        if (err) console.log(err);
+        var date = new Date(),
+            today = String(date.getDate()) + String(date.getMonth()) + String(date.getFullYear()),
+            mtime = String(stats.mtime.getDate()) + String(stats.mtime.getMonth()) + String(stats.mtime.getFullYear());
+        
+        if (mtime !== today) {
+            cramondTides(dataFile, function (err) {
+                if (err) console.log(err);
+                cb();
+            });
+        } else {
+            cb();
+        }
+    });
+}
+
+
+function readData() {
+    fs.readFile(dataFile, function (err, jsondata) {
+        if (err) return console.error(err);
+        
+        try {
+            data = JSON.parse(jsondata);
+            run();
+        } catch (err2) {
+            return new Error(err);
+        }
+    });
+}
+
+function run () {
+    http.createServer(function(req, res) {
+        var template;
+        fs.readFile(tpl, 'utf8', function (err, tplfile) {
+            if (err) throw new Error(err);
+            template = jade.compile(tplfile, {filename: tpl});
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(template({days: data}));
+        });
+    }).listen(port);
+
+    console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+}
+
+
+
+
+
+/*
 
 http.createServer(function(request, response) {
 
   var uri = url.parse(request.url).pathname,
       filename = path.join(process.cwd(), uri);
 
+  readData();
   fs.exists(filename, function(exists) {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
@@ -35,3 +107,4 @@ http.createServer(function(request, response) {
 }).listen(port);
 
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+*/
